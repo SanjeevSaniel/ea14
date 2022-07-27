0ede8858bafdfe536a0e2c6316971bc8a538c36e
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-07-28 00:58:12 +0530

    added content

---
+++ Js-notes/script.js
@@ -0,0 +1,9 @@
+// let b = 5;
+// // let c = ++b + b++;
+// let c = b++ + ++b;
+
+// console.log(c);
+// console.log(b)
+// console.log(!0 && "False");
+
+return (A % 10 == 0) && (B % 10 == 0) ? true : false;
