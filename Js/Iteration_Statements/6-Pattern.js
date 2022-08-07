3a2c5191a8b311196f2565669459fb01b8e390c9
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-08-07 10:57:52 +0530

    minor changes

--- Js/Iteration_Statements/6-Pattern.js
+++ Js/Iteration_Statements/6-Pattern.js
@@ -2,7 +2,6 @@
 Print the Pattern.
 
 Write a program which ask user for no of lines and print a pattern using an asterik .
-
 Note: You have to complete Print_pattern. No need to take any input.
 */
 
