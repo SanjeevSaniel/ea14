a73802b033decde4799f64430472188091bee0f6
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-08-17 07:57:10 +0530

    solved maps and sets Problem 2

--- Js/Maps_and_Sets/Problem-2.js
+++ Js/Maps_and_Sets/Problem-2.js
@@ -15,5 +15,15 @@ f=1"
 */
 
 const countAlphabet = (str) => {
-    
+  let count = 0;
+  let letterFrequency = new Map();
+  for (let i = 0; i < str.length; i++) {
+    if (letterFrequency.get(str[i])) {
+      letterFrequency.set(str[i], count + 1);
+    } else letterFrequency.set(str[i], 1);
+    count = letterFrequency.get(str[i]);
+  }
+  console.log(letterFrequency);
 };
+
+countAlphabet("abcadeecfb");
