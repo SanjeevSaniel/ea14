573704d8fba9629e6f8110656d3f29527aa8c55a
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-09-24 09:39:18 +0530

    Build the React Assignment - 1.

--- React/my-react-app/src/App.js
+++ React/my-react-app/src/App.js
@@ -2,22 +2,27 @@
 import FunctionalComponent from "./components/FunctionalComponent";
 import ClassComponent from "./components/ClassComponent";
 import "./components/components-styles.css";
+import { useState } from "react";
 
 function App() {
+  const [funcHidden, setFuncHidden] = useState(true);
+  const [clsHidden, setClsHidden] = useState(true);
+
   return (
     <div className="App">
       <h1 className="heading">Styling using Functional and Class Component</h1>
       <div className="btn-box">
-        <button className="btn btn--func">
+        <button className="btn btn--func" onClick={() => setFuncHidden(false)}>
           To see styling in Functional Component
         </button>
-        <button className="btn btn--cls">
+        <button className="btn btn--cls" onClick={() => setClsHidden(false)}>
           To see styling in Class Component
         </button>
       </div>
+
       <div className="component-box">
-        <FunctionalComponent />
-        <ClassComponent />
+        <>{!funcHidden ? <FunctionalComponent /> : null}</>
+        <>{!clsHidden ? <ClassComponent /> : null}</>
       </div>
     </div>
   );
