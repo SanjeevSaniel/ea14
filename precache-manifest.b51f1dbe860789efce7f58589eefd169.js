aae9b01e5ea767a8d70e0d05e516026e88bc268e
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-09-26 18:22:24 +0530

    Updates

---
+++ precache-manifest.b51f1dbe860789efce7f58589eefd169.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02f11392d4221c0d0ddf",
+    "url": "/react-gh-pages/static/css/main.73ace265.chunk.css"
+  },
+  {
+    "revision": "02f11392d4221c0d0ddf",
+    "url": "/react-gh-pages/static/js/main.02f11392.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/react-gh-pages/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "18836d79bd0a0930c4c1",
+    "url": "/react-gh-pages/static/js/2.18836d79.chunk.js"
+  },
+  {
+    "revision": "4a0d50d9b8b75ad33787",
+    "url": "/react-gh-pages/static/js/runtime~main.4a0d50d9.js"
+  },
+  {
+    "revision": "6263f36fbf8670ad6d0fe88fba6a51e4",
+    "url": "/react-gh-pages/index.html"
+  }
+];
