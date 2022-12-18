5a1d5555bb142160a3be725bab93e8d16d98f792
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-12-19 02:31:04 +0530

    Minor Code Changes

--- Js/Decision_Making/3-Eligible_Voter.js
+++ Js/Decision_Making/3-Eligible_Voter.js
@@ -6,8 +6,10 @@ Note: You have to complete isEligible function. No need to take any input
 */
 
 const isEligible = (a) => {
-  if (a >= 18) {
-    return "Eligible for Voting";
-  }
-  return "Not Eligible for Voting";
+  // if (a >= 18) {
+  //   return "Eligible for Voting";
+  // }
+  // return "Not Eligible for Voting";
+
+  return a >= 18 ? "Eligible for Voting" : "Not Eligible for Voting";
 };
